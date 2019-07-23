2de710e1c99c3c7877138622b53dac798eb7bb66
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-23 12:16:48 +0200

    added the route handling for login/logout/signup/get users

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../../data/secrets/secret.js');
 
 const Users = require('./user-model.js');
 
 const router = express.Router();
 
-// GET ALL USERS´J
-router.get('/users', async (req, res) => {
+const AuthMiddleware = require('../middleware/auth-middleware.js');
+
+// GET ALL USERS
+router.get('/users', AuthMiddleware.restricted, async (req, res) => {
   Users.find()
     .then(users => {
       res.status(200).json({
@@ -14,6 +19,96 @@ router.get('/users', async (req, res) => {
       });
     })
     .catch(error => res.status(500).json({ error: error }));
+});
+
+function usersWithoutPassword(users) {
+  return users.map(user => ({
+    id: user.id,
+    username: user.username,
+  }));
+}
+
+// ADD A NEW USER
+router.post('/register', (req, res) => {
+  let { username, password } = req.body;
+
+  if (username && password) {
+    const hash = bcrypt.hashSync(password, 12);
+    password = hash;
+
+    Users.add({ username, password })
+      .then(newUser => {
+        const token = generateJWT(user);
+
+        res.status(201).json({
+          id: newUser.id,
+          username: newUser.username,
+          token: token,
+        });
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json(
+            'There was an error during the creating of a new user. ' + error,
+          );
+      });
+  } else {
+    res
+      .status(400)
+      .json('Not all information were provided to create a new user.');
+  }
+});
+
+// LOGIN
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findByUsername(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateJWT(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token: token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials submitted.' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function generateJWT(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  const options = {
+    expiresIn: '1h',
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+// LOGOUT
+router.get('/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      error
+        ? res.json({
+            message: 'There was an error during the logout.',
+          })
+        : res.status(200).json({ message: 'Logout was successful!' });
+    });
+  } else {
+    res
+      .status(200)
+      .json({ message: 'Login did not happen. So logout is not necessary.' });
+  }
 });
 
 module.exports = router;
