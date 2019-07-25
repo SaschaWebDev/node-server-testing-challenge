4b2c7f80c09626cb64043a397f42fcc67990208a
Sascha Majewsky <sascha.majewsky@pm.me>
2019-07-25 21:01:29 +0200

    added at least two tests for each route handler and finished mvp

--- userModel.spec.js
+++ userModel.spec.js
@@ -32,4 +32,48 @@ describe('users model', () => {
       expect(user.username).toBe('christian');
     });
   });
+
+  describe('delete function', () => {
+    it('deletes users from the db', async () => {
+      let userAmount;
+      userAmount = await db('users');
+      expect(userAmount).toHaveLength(0);
+      await Users.add({
+        username: 'catdog',
+        password: bcrypt.hashSync('aloneintheworldisthelittlecatdog', 12),
+      });
+      await Users.add({
+        username: 'tourist',
+        password: bcrypt.hashSync('visitingcities', 12),
+      });
+      userAmount = await db('users');
+      expect(userAmount).toHaveLength(2);
+      await Users.remove(1);
+      userAmount = await db('users');
+      expect(userAmount).toHaveLength(1);
+    });
+
+    it('deletes users also after insertion from the db', async () => {
+      let userAmount;
+      userAmount = await db('users');
+      expect(userAmount).toHaveLength(0);
+      await Users.add({
+        username: 'catdog',
+        password: bcrypt.hashSync('aloneintheworldisthelittlecatdog', 12),
+      });
+      await Users.add({
+        username: 'tourist',
+        password: bcrypt.hashSync('visitingcities', 12),
+      });
+      userAmount = await db('users');
+      expect(userAmount).toHaveLength(2);
+      await Users.remove(1);
+      await Users.add({
+        username: 'tester3',
+        password: bcrypt.hashSync('tessster', 12),
+      });
+      userAmount = await db('users');
+      expect(userAmount).toHaveLength(2);
+    });
+  });
 });
